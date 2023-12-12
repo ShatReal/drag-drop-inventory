56335cb064ad6503cce0c840cbe38e4535d7afc8
Emily <shatteredrea@gmail.com>
2023-12-11 16:38:40 -0800

    Modify make_drag_preview to take in position

--- inventory_item.gd
+++ inventory_item.gd
@@ -19,19 +19,19 @@ func init(d: ItemData) -> void:
 
 # _at_position is not used because it doesn't matter where we click on
 # the inventory item.
-func _get_drag_data(_at_position: Vector2) -> Variant:
-	set_drag_preview(make_drag_preview())
+func _get_drag_data(at_position: Vector2) -> Variant:
+	set_drag_preview(make_drag_preview(at_position))
 	return self
 
 
-func make_drag_preview() -> Control:
+func make_drag_preview(at_position: Vector2) -> Control:
 	var t := TextureRect.new()
 	t.texture = texture
 	t.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	t.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
 	t.custom_minimum_size = size
 	t.modulate.a = 0.5
-	t.position = Vector2(-size/2)
+	t.position = Vector2(-at_position)
 
 	var c := Control.new()
 	c.add_child(t)
